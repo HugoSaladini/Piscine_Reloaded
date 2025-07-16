224a124a55c2f5d320236ed560863c0cb8a66d13
Hugo Saladini Da Silva <hg.saladini@gmail.com>
2025-07-16 18:27:07 -0300

    created util.c

---
+++ ex27/srcs/utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hda-silv <hda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 18:14:43 by hda-silv          #+#    #+#             */
+/*   Updated: 2025/07/16 18:25:28 by hda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		ft_putchar(str[index]);
+		index++;
+	}
+}
+
+void	ft_putendl(char *str)
+{
+	ft_putstr(str);
+	ft_putchar('\n');
+}
