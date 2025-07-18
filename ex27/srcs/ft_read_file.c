dc8ef2680178fd78033bcd07bafa5b779c3c4285
Hugo Saladini Da Silva <hg.saladini@gmail.com>
2025-07-18 17:29:57 -0300

    teste

---
+++ ex27/srcs/ft_read_file.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_read_file.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hda-silv <hda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 14:03:54 by hda-silv          #+#    #+#             */
+/*   Updated: 2025/07/18 17:27:57 by hda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include "../includes/ft_read_file.h"
+
+int	ft_open_file(char *file_name)
+{
+	int	fd;
+
+	fd = open(file_name, O_RDONLY);
+	return (fd);
+}
+
+void	ft_print_file(char *file_name)
+{
+	char	c;
+	int		fd;
+
+	fd = ft_open_file(file_name);
+	while (read(fd, &c, 1) > 0)
+	{
+		ft_putchar(c);
+	}
+	close(fd);
+}
---
+++ ex27/srcs/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hda-silv <hda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 16:41:42 by hda-silv          #+#    #+#             */
+/*   Updated: 2025/07/18 17:27:21 by hda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ft_utils.h"
+#include "../includes/ft_read_file.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc > 2)
+	{
+		ft_putendl("Too many arguments.");
+	}
+	else if (argc < 2)
+	{
+		ft_putendl("File name missing.");
+	}
+	else
+	{
+		ft_print_file(argv[1]);
+	}
+	return (0);
+}
