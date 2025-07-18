3020e85168aaba71396122c7cffa5c6c2dee12ab
Hugo Saladini Da Silva <hg.saladini@gmail.com>
2025-07-18 14:20:48 -0300

    teste

---
+++ ex27/includes/ft_read_file.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_read_file.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hda-silv <hda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/18 14:13:23 by hda-silv          #+#    #+#             */
+/*   Updated: 2025/07/18 14:17:03 by hda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_READ_FILE_H
+# define FT_READ_FILE_H
+
+int		ft_openfile(char *file_name);
+void	ft_print_file(char *file_name);
+
+#endif
